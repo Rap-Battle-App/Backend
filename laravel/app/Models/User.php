2bd34beb14e3536c26f87c97f9439cd5989eede9
Neop <mneop@web.de>
2015-11-03 17:13:51 +0100

    Models implemented

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -25,15 +25,71 @@ class User extends Model implements AuthenticatableContract,
 
     /**
      * The attributes that are mass assignable.
-     *
+     * TODO: add other fields, if necessary
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password', 'city', 'about_me'];
 
     /**
      * The attributes excluded from the model's JSON form.
      *
      * @var array
      */
-    protected $hidden = ['password', 'remember_token'];
+    protected $hidden = ['password', 'remember_token', 'email'];
+
+    /**
+     * Get votes of a user
+     */
+    public function votes()
+    {
+        returns $this->hasMany('App\Models\Vote');
+    }
+
+    /**
+     * Get battles where this user is rapper 1
+     */
+    public function battlesRapper1()
+    {
+        return $this->hasMany('App\Models\Battle', 'rapper1_id');
+    }
+
+    /**
+     * Get battles where this user is rapper 2
+     */
+    public function battlesRapper2()
+    {
+        return $this->hasMany('App\Models\Battle', 'rapper2_id');
+    }
+
+    /**
+     * Get battle requests, created by this user
+     */
+    public function battleRequestsChallenger()
+    {
+        return $this->hasMany('App\Models\BattleRequest', 'challenger_id');
+    }
+
+    /**
+     * Get battle requests, where this user is challenged
+     */
+    public function battleRequestsChallenged()
+    {
+        return $this->hasMany('App\Models\BattleRequest', 'challenged_id');
+    }
+
+    /**
+     * Get open battles where this user is rapper 1
+     */
+    public function openBattlesRapper1()
+    {
+        return $this->hasMany('App\Models\OpenBattle', 'rapper1_id');
+    }
+
+    /**
+     * Get open battles where this user is rapper 2
+     */
+    public function openBattlesRapper2()
+    {
+        return $this->hasMany('App\Models\OpenBattle', 'rapper2_id');
+    }
 }
