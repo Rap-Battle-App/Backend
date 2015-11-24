c7e8090fa96cb1a23e10ee002b005e8341d9c020
udeysingh95 <udeysingh95@gmail.com>
2015-11-24 09:08:27 +0100

    Updated Search controller
    Added the basic Php semantics, ->, removed the laravel automated part

--- laravel/app/Http/Controllers/SearchController.php
+++ laravel/app/Http/Controllers/SearchController.php
@@ -22,19 +22,13 @@ class SearchController extends Controller
             'search_string' => 'required|string'
         ]);
 
-        if ($validator->fails()) {
-            return redirect('post/create')
-                        ->withErrors($validator)
-                        ->withInput();
-        }
-       else{
            $user = App\User::findOrFail($search_string);            //might be a buggy line
            $profilePreview = array();
-           $profilePreview['user_id'] = $user['id']; 
-           $profilePreview['username'] = $user['name'];
-           $profilePreview['profile_picture'] = $user['picture'];
+           $profilePreview->user_id = $user->id; 
+           $profilePreview->username = $user->name;
+           $profilePreview->profile_picture = $user->picture;
             return $profilePreview;
-        }
+        
     }
 
     
