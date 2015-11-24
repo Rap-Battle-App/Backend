1c7ceeb203544eda948d1a4db15df35a9fb1fdf1
udeysingh95 <udeysingh95@gmail.com>
2015-11-24 10:04:24 +0100

    OpenBattleController
    With some more clarifications and the functionalities that marcus was
    saying, will add the concatenation and uploading soon. Need the link and
    methods to upload the files

--- laravel/app/Http/Controllers/OpenBattleController.php
+++ laravel/app/Http/Controllers/OpenBattleController.php
@@ -26,8 +26,12 @@ class OpenBattleController extends Controller
 
         
            $user_id = Auth::user()->user_id;
-           $battle = App\open_battles::findOrFail($id); 
+           $battle = App\open_battles::findOrFail($id);
+           
+           if($user_id == $battle->rapper2_id) 
            $Profile = App\User::findOrFail($battle->rapper2_id);
+           else
+           $Profile = App\User::findOrFail($battle->rapper1_id);
 
            $ProfileView = array();
            $ProfileView->user_id = $Profile->id;
@@ -45,9 +49,9 @@ class OpenBattleController extends Controller
 
 
            $PhaseInfo = array();
-           $ProfileView->time_left = $battle->phase_start;
-           $ProfileView->Phase1Info = $Phase1Info;
-           $ProfileView->Phase2Info = $Phase2Info;
+           $ProfileView->time_left = $battle->phase_start;      //need some work here
+           $PhaseInfo->Phase1Info = $Phase1Info;
+           $PhaseInfo->Phase2Info = $Phase2Info;
 
            $OpenBattle = array();
            $OpenBattle->id = $battle->id; 
@@ -66,21 +70,51 @@ class OpenBattleController extends Controller
         ]);
 
         
-          $id = Auth::user()->id;
-           $op_battle = App\open_battles::findOrFail($id);
-           $battle = App\battles::findOrFail($id);            
+           $id = Auth::user()->id;
+           $out_link = '/path/to/outputvideo.mp4';
+           $in_link = '/path/to/inputvideo1';
+           //$out_link = upload; sample
+           $event = new App\Events\VideoWasUploaded($out_link, [$in_link]);
+           Event::fire($event);
+
+           $link//get link
+
+           $op_battle = App\open_battles::findOrFail($id);            
            $battleRound = array();
-           $op_battle->beat1_id = $request->beat_id;
+           if($op_battle->phase == 1)
+           {
+              $op_battle->beat1_id = $request->beat_id;
+              if($op_battle->rapper1_round1 == NULL)
+              $op_battle->rapper1_round1 = $out_link;
+              else
+              $op_battle->rapper2_round1 = $out_link; 
+           }
+           else
+           {
+              $op_battle->beat2_id = $request->beat_id;
 
-           $battle->video = $request->video;
-           $battle->rapper1_id = $op_battle->rapper1_id;
-           $battle->rapper2_id = $op_battle->rapper2_id;
-           $battle->votes_rapper1 = 0;
-           $battle->votes_rapper2 = 0;
+              if($op_battle->rapper1_round2 == NULL)
+              $op_battle->rapper1_round2 = $out_link;
+              else
+              $op_battle->rapper2_round2 = $out_link;
+           }
+           $op_battle->phase++;
+           if($op_battle->phase == 3)   //both rounds are done and needs to close this open_battle entry and concatenate the video and add the final result to the battle table
+           {
+            $battle = new battles;
+            $battle->rapper1_id = $op_battle->rapper1_id;
+            $battle->rapper2_id = $op_battle->rapper2_id;
+            //concatenating video
 
+            //uploading video
+            $link = upload;
+            //final link
+            $battle->video = $link;
+            $battle->votes_rapper1 = 0;
+            $battle->votes_rapper2 = 0;
+           }
            
            $op_battle->save();
-           $battle->save();
         
     }
 
