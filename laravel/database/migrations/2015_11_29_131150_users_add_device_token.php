78a63246cd9d212e5ba0e6ca9e5c00b1b9d91d61
Chr0nX <chr0nx@live.de>
2015-11-29 14:15:59 +0100

    device_token migration

---
+++ laravel/database/migrations/2015_11_29_131150_users_add_device_token.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UsersAddDeviceToken extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('device_token')->nullable()->unique()->after('rating');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('device_token');
+        });
+    }
+}
