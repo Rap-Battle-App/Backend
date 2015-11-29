7706e1acaa75cde8e3c16ea24ba9ab8021309110
Chr0nX <chr0nx@live.de>
2015-11-29 14:52:56 +0100

    push notification messages

---
+++ laravel/resources/lang/de/push-notifications.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * The messages sent as push notifications.
+ */
+return [
+    'new-challenge' => ':opponent hat dich herausgefordert! Jetzt Battle annehmen.',
+    'opponent-round1' => ':opponent hat seine Hinrunde eingereicht, jetzt Rückrunde aufnehmen!',
+    'battle-published' => 'Dein Battle gegen :opponent ist komplett und damit zum Voting freigegeben!',
+    'battle-won' => 'Du hast das Battle gegen :opponent gewonnen! Sieh dir das Voting jetzt an.',
+    'battle-lost' => 'Du hast das Battle gegen :opponent verloren! Sieh dir das Voting jetzt an.'
+];
