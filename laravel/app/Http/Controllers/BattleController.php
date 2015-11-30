c0bc94f45a451eca01c209fd470c668ada9a8bc5
frodolin <frodo7219@gmx.de>
2015-11-30 12:18:39 +0100

    corrected coding style

--- laravel/app/Http/Controllers/BattleController.php
+++ laravel/app/Http/Controllers/BattleController.php
@@ -16,57 +16,53 @@ use App\Http\Controllers\Controller;
 class BattleController extends Controller
 {
 
-	public function __construct($AppName, IRequest $request){
-         parent::__construct($AppName, $request);
+    public function __construct($AppName, IRequest $request)
+    {
+        parent::__construct($AppName, $request);
     }
 
-	public function getBattle($battle_id)
-	{	
-		return response()->json(battle['battle' => Battle::findOrFail($battle_id)]);
-	}
+    public function getBattle($battle_id)
+    {	
+        return response()->json(battle['battle' => Battle::findOrFail($battle_id)]);
+    }
 
 	
-	public function getTrending(Request $request)
-	{
-		
-		return response()->json(battle['battle' => Battle::scopeTrending($request)]);
-
-	}
-	public function getOpenVoting(Request $request)
-	{
-		
-		return response()->json(battle['battle' => Battle::scopeOpenVoting($request)]);
-
-	}
-	public function getCompleted()
-	{
-
-		return response()->json(battle['battle' => Battle::scopeCompleted($request)]);
-
-	}
-	public function getOpen()
-	{
-
-		return response()->json(battle['openBattle' => OpenBattle::findAll()]);
-
-	}
-	public function postVote(Request $request, $battle_id)
-	{
-		
-		$validator = Validator::make($request->all(), [
+    public function getTrending(Request $request)
+    {
+        return response()->json(battle['battle' => Battle::scopeTrending($request)]);
+    }
+	
+    public function getOpenVoting(Request $request)
+    {
+        return response()->json(battle['battle' => Battle::scopeOpenVoting($request)]);
+    }
+	
+    public function getCompleted()
+    {
+        return response()->json(battle['battle' => Battle::scopeCompleted($request)]);
+    }
+	
+    public function getOpen()
+    {
+        return response()->json(battle['openBattle' => OpenBattle::findAll()]);
+    }
+	
+    public function postVote(Request $request, $battle_id)
+    {	
+        $validator = Validator::make($request->all(), [
             'rapper_number' => 'required|boolean'   
         ]);
 		
-		$user_id = Auth::user()->id;
-		$battle=getBattle($battle_id)
+        $user_id = Auth::user()->id;
+        $battle=getBattle($battle_id);
 				
-		//build new vote
+        //build new vote
         $vote = new Vote;
-		$vote->user_id = $user_id;
+        $vote->user_id = $user_id;
         $vote->battle_id = $battle->id;
         $vote->rapper_number = $request->rapper_number;
 				
-        vote->save();
+        $vote->save();
         // update vote counter
         if($vote->rapper_number == 0){
             $battle->votes_rapper1++;
@@ -77,9 +73,6 @@ class BattleController extends Controller
         // save vote count in battle
         $battle->save();
 			
-			
-
-
-	}
+    }
 
 }
