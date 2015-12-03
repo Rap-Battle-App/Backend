5772e5b379685b64a1183c1b1d631a09431def44
frodolin <frodo7219@gmx.de>
2015-12-03 11:40:56 +0100

    removed bugs

--- laravel/app/Http/Controllers/DataAccessController.php
+++ laravel/app/Http/Controllers/DataAccessController.php
@@ -2,29 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Model\Battle;
-use App\Http\Controllers\Controller;
-uae Illuminate\Http\Request;
+use App\Models\Battle;
+use Illuminate\Http\Request;
 
 class DataAccessController extends Controller
 {
-
     // returns the profile picture of a user identified by id
-	public function getPicture($id)
+    public function getPicture($id)
     {
-		$pathToFile = Auth::getUser($id)->picture;
-		
+        $pathToFile = Auth::getUser($id)->picture;
         return response()->download($pathToFile);
     }
 	
-	// returns the video of a battle identified by id
-	public function getVideo($id)
+    // returns the video of a battle identified by id
+    public function getVideo($id)
     {
-		$pathToFile = Battle::getBattle($id)->video;
-		
+        $pathToFile = Battle::getBattle($id)->video;
         return response()->download($pathToFile);
-        
-    }
-	
-	
+    }	
 }
