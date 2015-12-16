fb51c1797bc0d368e5070452e3f6b0921aecb384
udeysingh95 <udey.singh@hs-osnabrueck.de>
2015-12-16 19:06:23 +0100

    BattleRequest Controller
    Updated

--- laravel/app/Http/Controllers/BattleRequestController.php
+++ laravel/app/Http/Controllers/BattleRequestController.php
@@ -12,10 +12,21 @@ use App\Http\Controllers\Controller;
 
 class BattleRequestController extends Controller
 {
+    /*
+    |--------------------------------------------------------------------------
+    | Battle Request Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling requests of different rappers
+    | and getting challenges and provoke challenges, random opponents.
+    | basically regarding provoking battles.
+    |
+    */
+
     /**
-     * Display a listing of the resource.
+     * Handle a get Request demand, returns all the challengers to the Rapper
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
 
 
@@ -24,26 +35,24 @@ class BattleRequestController extends Controller
         $main_user = Auth::user()->user_id;
         $challengers = App::table('battles_requests')->where('challenged_id', $main_user)->get();
         
-        $Requests = array();
-        $i = 0;
-        while(array_pop($challengers)){
-        $profile = App\User::findOrFail($challengers->challenger_id);
-        $ProfilePreview = array();
-        $ProfilePreview->user_id = $profile->id;
-        $ProfilePreview->user_name = $profile->name;
-        $ProfilePreview->profile_picture = $profile->picture;
-        $Request[$i]->id = $challengers->id;
-        $Request[$i]->opponent = $ProfilePreview;
-        $Request[$i]->date = $profile->created_at;
-        $i++;  
-        }
-        $requests = $Requests;
-        $opponent_requests = $Requests
+        $profiles = $challengers->map(function($challengers, $key) {
+            return $challengers->profilePreview();
+        });
+        //$requests = $Requests;
+        //$opponent_requests = $Requests
 
-        return $requests;
-
+        return response()->json(['requests' => $profiles , 'opponent_requests' => $profiles]);
         
     }
+
+
+    /**
+     * Handle a challenge opponent request, create a new challenge in the requests table
+     * with his id and opponent id 
+     *
+     * @param  \Illumnate\Http\Request  user_id
+     * @return void
+     */
     public function postRequests(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -59,18 +68,52 @@ class BattleRequestController extends Controller
            $battles_requests->save();
         
     }
+
+
+    /**
+     * Handle a battle request responses, if both agree, open a new entry in the OpenBattle table
+     * otherwise, simply removes the entry from the battle requests table
+     *
+     * @param  \Illumnate\Http\id   [battle request table]
+     * @param  \Illumnate\Http\Request  accepted   
+     * @return void
+     */
     public function postAnswer(Request $request , $id)
     {
-       /* $validator = Validator::make($request->all(), [
+        $validator = Validator::make($request->all(), [
             'accepted' => 'required|boolean'
         ]);
 
-        
-           $battle = App\battles_requests::findOrFail($id);           
-           $battle->accepted = $request->accepted;
+           $battle = battles_requests::findOrFail($id);
+           //accepted response opens a new battle and removes the request from the battle request table
+           if($request->accepted == TRUE){
+            
+            $op_battle = new open_battles;
+            $op_battle->rapper1_id = $battle->challenger_id;
+            $op_battle->rapper2_id = $battle->challenged_id;
+            $op_battle->phase = 0;
+            $op_battle->beat1_id = 0;
+            $op_battle->rapper1_round1 = NULL;
+            $op_battle->rapper2_round1 = NULL;
+            $op_battle->beat2_id = 0;
+            $op_battle->rapper1_round2 = NULL;
+            $op_battle->rapper2_round2 = NULL;
+            $op_battle->save();
+           }
+           
+           //deletes the entry from the battel request table, whatever the response is         
            $battle->save();
-        */
+        
     }
+
+
+    /**
+     * Handles the request for the random opponent demand,
+     * returns a random opponent accoring to the ranking of the challenger  
+     *
+     * @param  void
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getRandomOpponent()
     {
            $user = Auth::user()->user_id;       
@@ -78,11 +121,11 @@ class BattleRequestController extends Controller
 
            $profile = $rand_coll->random()->first();      //selected one from the random collection
            
-           $profilePreview = array();
-           $profilePreview->user_id = $profile->id; 
-           $profilePreview->username = $profile->name;
-           $profilePreview->profile_picture = $profile->picture;    //needs the file system insertion methods 
-          return $profilePreview;
+           $profiles = $profile->map(function($profile, $key) {
+            return $profile->profilePreview();
+           });
+            
+          return response()->json(['opponent' => $profiles]);
         
     }
 
