b09ee4f2272fc83e5b12ec2836f856ce049d0311
Neop <mneop@web.de>
2015-12-22 12:17:34 +0100

    BattleController fixed

--- laravel/app/Http/Controllers/BattleController.php
+++ laravel/app/Http/Controllers/BattleController.php
@@ -3,53 +3,53 @@
 namespace App\Http\Controllers;
 
 use Auth;
-use App\Models\Battle;
-use App\Models\User;
 use Illuminate\Http\Request;
-use App\Http\Requests;
+
+use App\Models\User;
+use App\Models\Battle;
 use App\Models\OpenBattle;
-
-
-//todo: correct api response format 
-
-//use Illuminate\Support\Facades\Auth;
+use App\Models\Vote;
 
 class BattleController extends Controller
 {
 
     //return a single Battle Object identified by id
     public function getBattle($id) 
-    {	
-        $battleInfo = Battle::findOrFail($id);
+    {
+        $battle = Battle::findOrFail($id);
+        $user = Auth::user();
 
         $profilePreview1 = array(
-            'user_id' => $battleInfo->rapper1_id,
-            'username' => User::findOrFail($battleInfo->rapper1_id)->username,
-            'profile_picture' => User::findOrFail($battleInfo->rapper1_id)->picture,
-	);
+            'user_id' => $battle->rapper1_id,
+            'username' => User::findOrFail($battle->rapper1_id)->username,
+            'profile_picture' => User::findOrFail($battle->rapper1_id)->picture,
+    	);
 
         $profilePreview2 = array(
-            'user_id' => $battleInfo->rapper2_id,
-            'username' => User::findOrFail($battleInfo->rapper2_id)->username,
-            'profile_picture' => User::findOrFail($battleInfo->rapper2_id)->picture,
-	);
+            'user_id' => $battle->rapper2_id,
+            'username' => User::findOrFail($battle->rapper2_id)->username,
+            'profile_picture' => User::findOrFail($battle->rapper2_id)->picture,
+	    );
+
+        $userVote = $user->votes()->where('battle_id', $battle->id)->get()->first();
+        $votedFor = ($userVote == null ? null : $userVote->rapper_number);
 
         $voting = array(
-            'votes_rapper1' => $battleInfo->votes_rapper1,
-            'votes_rapper2' => $battleInfo->votes_rapper2,
-            'voted_for' => '', // TODO: who did the user vote for?
-            'open' => $battleInfo->isOpenVoting(),
-	);
+            'votes_rapper1' => $battle->votes_rapper1,
+            'votes_rapper2' => $battle->votes_rapper2,
+            'voted_for' => $votedFor,
+            'open' => $battle->isOpenVoting(),
+	    );
 
-        $battle = array(
-            'id' => $battleInfo->id,
-            'video_url' => $battleInfo->video,
+        $battleInfo = array(
+            'id' => $battle->id,
+            'video_url' => $battle->video,
             'rapper1' => $profilePreview1,
             'rapper2' => $profilePreview2,
             'voting' => $voting,
         );
         
-        return $battle;
+        return response()->json($battleInfo);
     }
 	
     //return an Array of Battles with the most votes
@@ -61,92 +61,76 @@ class BattleController extends Controller
     //return an Array of Battles that are still open for voting
     public function getOpenVoting(Request $request)
     {
-        //check if request contains user id (if yes return only battles by that user)
-        if($request->input('user_id'))
-        {
-            return Battle::openVoting()->where(function($query){
-                $query->where('rapper1_id', '==', $request->input('user_id'))
-                      ->orWhere('rapper2_id', '==', $request->input('user_id'));
-        });
-
+        if($request->has('user_id')){
+            // get battles by user
+            $user_id = $request->input('user_id');
+            $battles = User::find($user_id)->battles()->openVoting();
+        } else {
+            // get all battles
+            $battles = Battle::openVoting();
         }
-        else
-        {
-            return Battle::openVoting()->paginate($request->input('amount'));
-        }
+ 
+        return $battles->paginate($request->input('amount'));
     }
 	
     //return all Battles that are no longer open for voting for the current user
-    public function getCompleted()
+    public function getCompleted(Request $request)
     {
-        //check if request contains user id (if yes return only battles by that user)
-        //return response()->json(Auth::user()->battles()->completed()->paginate($request->input('amount')));
-        /*
-        if($request->input('user_id'))
-        {
-            return response()->json(Battle::completed()->where(function($query){
-                $query->where('rapper1_id', '==', $request->input('user_id'))
-                      ->orWhere('rapper2_id', '==', $request->input('user_id'));
-        }));
-
+        if($request->has('user_id')){
+            // get battles by user
+            $user_id = $request->input('user_id');
+            $battles = User::find($user_id)->battles()->completed();
+        } else {
+            // get all battles
+            $battles = Battle::completed();
         }
-        else
-        {
-            //return Battle::completed()->paginate($request->input('amount'));
-            //return response()->json(Battle::completed()->paginate($request->input('amount')));
-            return response()->json(Battle::completed());
-        }
-*/
-        return response()->json(Battle::completed());
-        //return response()->json(Battle::completed()->paginate($request->input('amount')));
-
-        //return response()->json(Battle::completed());
-        //return Battle::completed();   //routing error for unknown reasons
-        
+ 
+        return $battles->paginate($request->input('amount'));
     }
 	
     //return an Array of all openBattles for the current user
     public function getOpen()
     {
-        //todo: check if request contains user id (if yes return only battles by that user)
-        //return response()->json(Auth::user()->battles()->open());
-        //return response()->json(Battle::open());
-        //return response()->json(OpenBattle::all());
-
-        /*return OpenBattle::all()->where(function($query){
-                $query->where('rapper1_id', '==', $request->input('user_id'))
-                      ->orWhere('rapper2_id', '==', $request->input('user_id'));
-        });*/
-        return OpenBattle::all();
+        // get open battles by user
+        $battles = Auth::user()->openBattles();
+ 
+        return $battles->paginate();
     }
 	
     //increase the votes of a single rapper in one battle identified by id 
     public function postVote(Request $request, $battle_id)
     {	
-        $validator = $this->validate($request->all(), [
+        $this->validate($request, [
             'rapper_number' => 'required|integer'   
         ]);
-		
-        $user_id = Auth::user()->id;
-        $battle=Battle::find($battle_id);
-				
-        //build new vote
-        $vote = new Vote;
-        $vote->user_id = $user_id;
-        $vote->battle_id = $battle->id;
-        $vote->rapper_number = $request->input('rapper_number');
-				
-        $vote->save();
-        // update vote counter
-        if($vote->rapper_number == 1){
-            $battle->votes_rapper1++;
+
+        $user = Auth::user();
+        $battle = Battle::find($battle_id);
+
+        // check if battle is votable
+        if(!$battle->isOpenVoting()) return response('', 405); // Method not allowed
+
+        // don't let the user change a vote
+        if($user->votes()->where('battle_id', $battle->id)->get()->first() == null){
+            // build new vote
+            $vote = new Vote;
+            $vote->user_id = $user->id;
+            $vote->battle_id = $battle->id;
+            $vote->rapper_number = $request->input('rapper_number');
+
+            // update vote counter
+            if($vote->rapper_number == 0){
+                $battle->votes_rapper1++;
+            } else if($vote->rapper_number == 1){
+                $battle->votes_rapper2++;
+            } else return response('', 422); // Unprocessable Entity
+
+            // save vote count in battle
+            $vote->save();
+            $battle->save();
         } else {
-            $battle->votes_rapper2++;
+            return response('', 405); // Method not allowed
         }
-		
-        // save vote count in battle
-        $battle->save();
-			
     }
 
 }
